1e502d7f06ae7c74445177c7c8c53a8c07ba1cd1
robertoluis77-cloud <robertoluis77@gmail.com>
2026-01-27 14:07:43 -0600

    Skipping google tc

--- e2e/tests/googleFindElement.spec.ts
+++ e2e/tests/googleFindElement.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "playwright/test";
 
-test('test', async ({ page }) => {
+test.skip('test', async ({ page }) => {
   await page.goto('https://www.google.com/');
   //await expect(page.getByRole('combobox', { name: 'Buscar' })).toBeVisible();
   await page.getByRole('combobox', { name: 'Buscar' }).click();
