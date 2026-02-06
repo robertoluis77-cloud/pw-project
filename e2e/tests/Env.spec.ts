8f8ddcbafc0c1f6ceef5362ee803fce72d8e0c09
robertoluis77-cloud <robertoluis77@gmail.com>
2026-02-06 14:42:16 -0600

    Env file created and working

--- e2e/tests/Env.spec.ts
+++ e2e/tests/Env.spec.ts
@@ -3,12 +3,13 @@ import { Environment } from "./Utils/envConfig";
 
 const env = Environment.getConfig();
 
-//const BASE_URL = env.getConfig().baseURL;
+const BASE_URL = env.baseURL;
 //const API_URL = env.getConfig().apiURL;
 //const FRT_BASE_URL = env.getConfig().frtBaseURL;
 
 test('', async ({ page }) => {
     console.log('Env: ', env);
+    console.log('\nBase URL: ', BASE_URL);
 })
 
 
